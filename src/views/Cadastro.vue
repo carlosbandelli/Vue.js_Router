8d78ccfbaa747d7a2e56201683256fa8b0accdd3
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-07-05 22:51:13 -0300

    Adicionadno rotas

---
+++ src/views/Cadastro.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h4>Rota Pai</h4>
+        <input type="text" name="email" id="email" placeholder="Email..."><br>
+        <input type="text" class="button" name="senha" id="senha" placeholder="******"><br>
+        <button id='button'>Testar</button>
+        <h4>{{parametroDaRota}}</h4>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    created(){
+       // this.parametroDaRota = this.$route.params.dado
+    },
+    data(){
+        return{
+            parametroDaRota: "Nada"
+        }
+    }
+}
+
+</script>
+
+
+
+<style scoped>
+#button{
+    margin-top: 0.5%
+}
+.button{
+    margin-top: 0.5%
+}
+
+</style>
